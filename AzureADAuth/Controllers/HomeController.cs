58d80dc3eb1421f7d6afa04a62558396237cf10f
aizhan <ayzhan.karasartova@sibers.com>
2024-04-23 11:07:23 +0600

    update keys

--- AzureADAuth/Controllers/HomeController.cs
+++ AzureADAuth/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Web;
 using System.Web.Mvc;
 
@@ -15,8 +16,13 @@ namespace AzureADAuth.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            // Check
+            var isAuth = User.Identity.IsAuthenticated;
 
+            var identity = (ClaimsIdentity)User.Identity;
+            var emailClaim = identity.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Email)?.Value;
+
+            ViewBag.Message = "Your application description page.";
             return View();
         }
 
